f51996ee147a0562077432e0bc10e0056e8e9cfe
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-03 16:37:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainRemover = document.querySelector('main');
+        mainRemover.remove();
+
+const newHeader = document.createElement("h1");
+        newHeader.id = 'victory';
+        newHeader.innerHTML = "EARNEST is the champion";
